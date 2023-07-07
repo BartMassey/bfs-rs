0bcf295460fa4323b851466215a54c24f3f3d322
Bart Massey <bart.massey@gmail.com>
2023-07-06 23:42:46 -0700

    cleaned up

--- bfs.rs
+++ bfs.rs
@@ -8,9 +8,9 @@ use anyhow::anyhow;
 
 type Edge = Result<(u32, u32), anyhow::Error>;
 
+/// Read the graph.  Return the edges as tuples. Return
+/// errors lazily.
 fn read_edges(csv: Reader<File>) -> impl Iterator<Item = Edge> {
-    // Read the graph.  Iterate over the edges
-    // as tuples.
     csv
         .into_records()
         .map(|r| {
@@ -25,8 +25,8 @@ fn read_edges(csv: Reader<File>) -> impl Iterator<Item = Edge> {
 
 type Graph = HashMap<u32, HashSet<u32>>;
 
+/// Build the adjacency map. Ensure bidirectionality.
 fn build_graph(edges: impl Iterator<Item = Edge>) -> Result<Graph, anyhow::Error> {
-    // Build the adjacency map. Ensure bidirectionality.
     let mut graph: Graph = HashMap::new();
     for edge in edges {
         // Stop on broken edge.
@@ -35,6 +35,35 @@ fn build_graph(edges: impl Iterator<Item = Edge>) -> Result<Graph, anyhow::Error
         graph.entry(end).or_default().insert(start);
     }
     Ok(graph)
+}
+
+/// Breadth-First Search the graph.
+fn bfs(graph: &Graph, init: u32, goal: u32) -> Result<Option<Vec<u32>>, anyhow::Error> {
+    let mut q = VecDeque::from([init]);
+    // Keep the parent of each node along the shortest path.
+    // If the node is encountered later, it must not be
+    // along a shortest path.
+    let mut parents = HashMap::from([(init, None)]);
+    while let Some(mut node) = q.pop_front() {
+        if node == goal {
+            println!("path found");
+            let mut path = vec![node];
+            while let Some(parent) = parents[&node] {
+                path.push(parent);
+                node = parent;
+            }
+            path.reverse();
+            return Ok(Some(path));
+        }
+        for &child in graph.get(&node).ok_or(anyhow!("no node {}", node))?.iter() {
+            if parents.contains_key(&child) {
+                continue;
+            }
+            parents.insert(child, Some(node));
+            q.push_back(child);
+        }
+    }
+    Ok(None)
 }
 
 
@@ -56,34 +85,14 @@ fn main() -> Result<(), anyhow::Error> {
     // Build the graph.
     let graph = build_graph(edges)?;
 
-    // BFS
-    let mut q = VecDeque::from([args.init]);
-    // Keep the parent of each node along the shortest path.
-    // If the node is encountered later, it must not be
-    // along a shortest path.
-    let mut parents = HashMap::from([(args.init, None)]);
-    while let Some(mut node) = q.pop_front() {
-        if node == args.goal {
-            println!("path found");
-            let mut path = vec![node];
-            while let Some(parent) = parents[&node] {
-                path.push(parent);
-                node = parent;
-            }
-            for p in path.iter().rev() {
-                println!("{}", p);
-            }
-            return Ok(());
+    // Show the solution.
+    if let Some(path) = bfs(&graph, args.init, args.goal)? {
+        for p in path.iter() {
+            println!("{}", p);
         }
-        for &child in graph.get(&node).ok_or(anyhow!("no node {}", node))?.iter() {
-            if parents.contains_key(&child) {
-                continue;
-            }
-            parents.insert(child, Some(node));
-            q.push_back(child);
-        }
+    } else {
+        println!("no path found");
     }
 
-    println!("no path found");
     Ok(())
 }    
